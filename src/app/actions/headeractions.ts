c0ed74bb5338f92f696812f1a477f12cc748afc8
Vinay Joseph <vjoseph@opentext.com>
2023-10-26 22:02:51 +1100

    Reshaping dropdown

--- src/app/actions/headeractions.ts
+++ src/app/actions/headeractions.ts
@@ -5,3 +5,13 @@ export const addSelectedDatabases = createAction(
   '[Database] Add Selected',
   props<{ databases: string[] }>()
 );
+
+export const showVectorResults = createAction(
+  '[Vector] Show Results',
+  props<{ show: boolean }>()
+);
+
+export const showIDOLResults = createAction(
+  '[IDOL] Show Results',
+  props<{ show: boolean }>()
+);
