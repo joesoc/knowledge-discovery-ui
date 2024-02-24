c67b2f45f4cc83649d7be861f2907b732a32457d
Vinay Joseph <vjoseph@opentext.com>
2024-02-24 21:01:07 +1100

    Chatbot working prod test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,12 @@
 export const environment = {
     production: true,
+    answerserver_fqdn: 'localhost',
+    answerserver_port: '4200',
+    answerserver_scheme: 'https',
+    api:'api',
     qms_fqdn: 'qms.idoldemos.net',
     qms_port: '16000',
     dah_fqdb: 'dah.idoldemos.net',
-    dah_port: '9060',
-    answerserver_fqdn: 'answerserver.idoldemos.net',
-    answerserver_port: '12000',
-    apiPrefix: ''
-};
+    dah_port: '9060'
+  };
+  
