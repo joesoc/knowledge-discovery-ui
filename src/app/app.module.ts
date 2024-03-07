a6597c3e384c8332062e1a814de1f598c510b734
Ashley Hunter <ahunter2@opentext.com>
2024-03-07 15:20:30 +0000

    lots of ui updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgIconsModule } from '@ng-icons/core';
+import {
+  lucideAlignLeft,
+  lucideAlignRight,
+  lucideCheck,
+  lucideDatabase,
+  lucideMaximize2,
+  lucideMessageCircle,
+  lucideMinimize2,
+  lucideSearch,
+  lucideSend,
+  lucideSettings,
+  lucideX,
+} from '@ng-icons/lucide';
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './home/home.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { ResultsCountComponent } from './landing-page/results-count/results-count.component';
-import { SearchResultsComponent } from './landing-page/search-results/search-results.component';
-import { SearchResultItemComponent } from './landing-page/search-result-item/search-result-item.component';
-import { StoreModule } from '@ngrx/store';
-import { selectedDatabaseReducer } from './reducers/headerreducer';
-import { SettingsDialogComponent } from './shared/header/settings-dialog/settings-dialog.component';
 import { AnswerpaneComponent } from './landing-page/answerpane/answerpane.component';
+import { ChatSettingsComponent } from './landing-page/chat/chat-settings/chat-settings.component';
+import { ChatUrlDirective } from './landing-page/chat/chat-url/chat-url.directive';
 import { ChatComponent } from './landing-page/chat/chat.component';
 import { DynamicValidChoicesComponent } from './landing-page/chat/dynamic-valid-choices/dynamic-valid-choices.component';
-import { ChatUrlDirective } from './landing-page/chat/chat-url/chat-url.directive';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ChatSettingsComponent } from './landing-page/chat/chat-settings/chat-settings.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ResultsCountComponent } from './landing-page/results-count/results-count.component';
+import { SearchResultItemComponent } from './landing-page/search-result-item/search-result-item.component';
+import { SearchResultsComponent } from './landing-page/search-results/search-results.component';
+import { selectedDatabaseReducer } from './reducers/headerreducer';
+import { HeaderComponent } from './shared/header/header.component';
+import { SettingsDialogComponent } from './shared/header/settings-dialog/settings-dialog.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +48,7 @@ import { ChatSettingsComponent } from './landing-page/chat/chat-settings/chat-se
     ChatComponent,
     DynamicValidChoicesComponent,
     ChatUrlDirective,
-    ChatSettingsComponent
+    ChatSettingsComponent,
   ],
   imports: [
     BrowserModule,
@@ -42,9 +56,22 @@ import { ChatSettingsComponent } from './landing-page/chat/chat-settings/chat-se
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    StoreModule.forRoot({selectedDatabases: selectedDatabaseReducer}, {})
+    StoreModule.forRoot({ selectedDatabases: selectedDatabaseReducer }, {}),
+    NgIconsModule.withIcons({
+      lucideSettings,
+      lucideDatabase,
+      lucideSearch,
+      lucideAlignLeft,
+      lucideAlignRight,
+      lucideCheck,
+      lucideMinimize2,
+      lucideMaximize2,
+      lucideX,
+      lucideMessageCircle,
+      lucideSend,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/landing-page/chat/chat.component.ts
+++ src/app/landing-page/chat/chat.component.ts
@@ -1,43 +1,65 @@
-import { ChangeDetectorRef, Component, ComponentFactoryResolver, ElementRef, EventEmitter, Input, Output, QueryList, ViewChild, ViewChildren, inject } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+  inject,
+} from '@angular/core';
+import { DomSanitizer, SafeHtml, SafeUrl } from '@angular/platform-browser';
 import { Prompt } from 'src/app/interfaces/IAnswerServerConversationPrompts';
 import { IManageResourcesResponse } from 'src/app/interfaces/IAnswerServerConversationResponse';
 import { AnswerService } from 'src/app/services/answer.service';
-import { DomSanitizer, SafeHtml, SafeUrl } from '@angular/platform-browser';
-import { environment } from 'src/environments/environment.prod';
 import { DataService } from 'src/app/services/data.service';
 import { IndexedDbService } from 'src/app/services/indexed-db.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit, AfterViewInit {
   private changeDetector = inject(ChangeDetectorRef);
-  sessionID: string = "";
-  messages: Array<{ username: string; timestamp: string ; text: SafeHtml; fromUser: boolean, choices: string[] }> = [];
+  sessionID: string = '';
+  messages: Array<{
+    username: string;
+    timestamp: string;
+    text: SafeHtml;
+    fromUser: boolean;
+    choices: string[];
+  }> = [];
+
   previewUrl?: SafeUrl;
   rawUrl?: string;
   showChatSettings: boolean = false;
-  answer_source: string = "";
-  answer_text: string = "";
+  answer_source: string = '';
+  answer_text: string = '';
   @Output() closeChat = new EventEmitter<void>();
 
   @ViewChildren('chatMessage') chatMessages!: QueryList<ElementRef<HTMLElement>>;
+
+  @ViewChild('messageInput')
+  messageInput!: ElementRef<HTMLInputElement>;
 
   onCloseButtonClick() {
     this.closeChat.emit();
   }
   showChat = true;
-  isMinimized =false;
-  newMessage = {username: 'User1', timestamp: new Date(), text: ''};
-  constructor(private answerService: AnswerService, 
-              private sanitizer: DomSanitizer, 
-              private redisService: DataService,
-              private indexDBService: IndexedDbService,
-              private componentFactoryResolver: ComponentFactoryResolver) { 
+  isMinimized = false;
+  newMessage = { username: 'User1', timestamp: new Date(), text: '' };
+  constructor(
+    private answerService: AnswerService,
+    private sanitizer: DomSanitizer,
+    private redisService: DataService,
+    private indexDBService: IndexedDbService
+  ) {}
 
-  }
   toggleChatSettings(): void {
     this.showChatSettings = !this.showChatSettings;
   }
@@ -55,36 +77,39 @@ export class ChatComponent {
     // Logic to show the entire chat window
   }
 
-  
   ngOnInit(): void {
     this.initializeChatbot();
   }
-  
+
+  ngAfterViewInit(): void {
+    // Focus on the message input when the component is initialized
+    this.messageInput.nativeElement.focus();
+  }
+
   initializeChatbot(): void {
     // Replace 'variables' with any required session variables
-        this.answerService.getSessionID().subscribe(async (response: IManageResourcesResponse) => {
-          this.sessionID = response.autnresponse.responsedata.result.managed_resources.id;
-          let database = await this.indexDBService.getItem('selectedDatabase');
-          let answerServerSystem = await this.indexDBService.getItem('selectedAnswerSystem');
-          this.redisService.addDataToRedis(this.sessionID, database.value, answerServerSystem.value);
-          this.answerService.converse(this.sessionID, '').subscribe((response) => {
-            let prompts:Prompt[] = response.autnresponse.responsedata.prompts;
-            prompts.forEach((prompt) => {
-              this.addMessage("Conversation Server", prompt.prompt, false);
-            });
-          });
-          //this.addMessage('Hello! How can I help you today?'+this.sessionID, false);
+    this.answerService.getSessionID().subscribe(async (response: IManageResourcesResponse) => {
+      this.sessionID = response.autnresponse.responsedata.result.managed_resources.id;
+      let database = await this.indexDBService.getItem('selectedDatabase');
+      let answerServerSystem = await this.indexDBService.getItem('selectedAnswerSystem');
+      this.redisService.addDataToRedis(this.sessionID, database.value, answerServerSystem.value);
+      this.answerService.converse(this.sessionID, '').subscribe(response => {
+        let prompts: Prompt[] = response.autnresponse.responsedata.prompts;
+        prompts.forEach(prompt => {
+          this.addMessage('Conversation Server', prompt.prompt, false);
         });
-      }
-        // Example method to add a message
-  addMessage(username:string, text: SafeHtml, fromUser: boolean, choices: string[] = []) {
-
+      });
+      //this.addMessage('Hello! How can I help you today?'+this.sessionID, false);
+    });
+  }
+  // Example method to add a message
+  addMessage(username: string, text: SafeHtml, fromUser: boolean, choices: string[] = []) {
     const message = {
       username: username,
       timestamp: new Date().toLocaleTimeString(),
       text: text,
       fromUser: fromUser,
-      choices: choices
+      choices: choices,
     };
 
     this.messages.push(message);
@@ -94,104 +119,117 @@ export class ChatComponent {
 
   scrollToBottom(): void {
     try {
-      this.chatMessages.last.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'end' });
+      this.chatMessages.last.nativeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+      });
     } catch (error) {}
   }
 
-  loadValidChoices(validChoices: string[]){
-    // const componentFactory = this.componentFactoryResolver.resolveComponentFactory(DynamicValidChoicesComponent);
-    // const viewContainerRef = this.dynamicHost.viewContainerRef;
-    // viewContainerRef.clear();
-    // const componentRef = viewContainerRef.createComponent(componentFactory);
-    // (<DynamicValidChoicesComponent>componentRef.instance).validChoices = validChoices;
-  }
-
   decodeHtml(html: string) {
-    var txt = document.createElement("textarea");
+    var txt = document.createElement('textarea');
     txt.innerHTML = html;
     return txt.value;
-}
+  }
 
   sendMessage() {
     const startTime = Date.now(); // Capture start time
     const sessionIDInputHTML: SafeHtml = `<input type="hidden" name="sessionID" value="${this.sessionID}">`;
 
-    const userMessage = { username: 'User', timestamp: new Date().toLocaleTimeString(), text: this.newMessage.text, fromUser: true };
-    this.addMessage("User", this.newMessage.text, true);
-    this.newMessage.text = "";
+    const userMessage = {
+      username: 'User',
+      timestamp: new Date().toLocaleTimeString(),
+      text: this.newMessage.text,
+      fromUser: true,
+    };
+    this.addMessage('User', this.newMessage.text, true);
+    this.newMessage.text = '';
 
-    const messagesToCheck = ['What is your question?', 
-                             'Did that answer your question?', 
-                             'Whats next?',
-                             '<p>I don\'t have any answers</p>',
-                             'OK, let\'s cancel that.',
-                             'Welcome to Virtual Agent',
-                             'Thank you and have a nice day...',
-                             'Let me look up other answers for you']; // Add your messages here
-    const messageToAvoidAddingSessionID = ['Ask another question',
-  'Summarize the document','End'];
-    let usertext = "";
+    const messagesToCheck = [
+      'What is your question?',
+      'Did that answer your question?',
+      'Whats next?',
+      "<p>I don't have any answers</p>",
+      "OK, let's cancel that.",
+      'Welcome to Virtual Agent',
+      'Thank you and have a nice day...',
+      'Let me look up other answers for you',
+    ]; // Add your messages here
+    const messageToAvoidAddingSessionID = ['Ask another question', 'Summarize the document', 'End'];
+    let usertext = '';
     if (messageToAvoidAddingSessionID.includes(userMessage.text)) {
       usertext = userMessage.text;
-    }
-    else{
+    } else {
       usertext = userMessage.text + `|${this.sessionID}`;
     }
-    this.answerService.converse(this.sessionID, usertext).subscribe((response) => {
+    this.answerService.converse(this.sessionID, usertext).subscribe(response => {
       let prompts: Prompt[] = response.autnresponse.responsedata.prompts;
-      prompts.forEach((prompt) => {
-
+      prompts.forEach(prompt => {
         const endTime = Date.now(); // Capture end time
         const duration = (endTime - startTime) / 1000; // Calculate duration in seconds
 
         if (!messagesToCheck.includes(prompt.prompt)) {
           const doc = new DOMParser().parseFromString(prompt.prompt, 'text/html');
-          const hiddenValue = doc.querySelector('input[id="sourceHiddenField"]') as HTMLInputElement;
+          const hiddenValue = doc.querySelector(
+            'input[id="sourceHiddenField"]'
+          ) as HTMLInputElement;
           if (hiddenValue !== null) {
             console.log(hiddenValue.value);
             this.answer_source = hiddenValue.value;
           }
-          const texthiddenValue = doc.querySelector('input[id="textHiddenField"]') as HTMLInputElement;
+          const texthiddenValue = doc.querySelector(
+            'input[id="textHiddenField"]'
+          ) as HTMLInputElement;
           if (texthiddenValue !== null) {
             console.log(texthiddenValue.value);
             this.answer_text = texthiddenValue.value;
           }
-          const safeMessage: SafeHtml = this.sanitizer.bypassSecurityTrustHtml(this.decodeHtml(prompt.prompt) + "<br><span class=\"responded-time\">Responded in " + duration + " seconds</span>");
-          this.addMessage("Conversation Server", safeMessage, false);
-        }
-        
-        else {
+          const safeMessage: SafeHtml = this.sanitizer.bypassSecurityTrustHtml(
+            this.decodeHtml(prompt.prompt) +
+              '<br><span class="responded-time">Responded in ' +
+              duration +
+              ' seconds</span>'
+          );
+          this.addMessage('Conversation Server', safeMessage, false);
+        } else {
           if (prompt.valid_choices) {
             let response = prompt.prompt;
             // this.loadValidChoices(prompt.valid_choices.valid_choice);
             let responseHTML = `${response}<br>`;
             let safeMessage: SafeHtml = this.sanitizer.bypassSecurityTrustHtml(responseHTML);
-            this.addMessage("Conversation Server", safeMessage, false, prompt.valid_choices.valid_choice);
-          }
-          else{
+            this.addMessage(
+              'Conversation Server',
+              safeMessage,
+              false,
+              prompt.valid_choices.valid_choice
+            );
+          } else {
             let safeMessage: SafeHtml = this.sanitizer.bypassSecurityTrustHtml(prompt.prompt);
-            this.addMessage("Conversation Server", safeMessage, false);
+            this.addMessage('Conversation Server', safeMessage, false);
           }
         }
-
       });
     });
   }
-  
-  
+
   selectChoice(choice: string): void {
     this.newMessage.text = choice;
     this.newMessage.timestamp = new Date();
     this.sendMessage();
   }
-  
+
   showPreview(url: string) {
     this.rawUrl = url;
     // TODO: remove this hard coded url
-    url = `?Action=View&NoACI=true&Reference=${encodeURIComponent(url)}&EmbedImages=true&StripScript=true&OriginalBaseURL=true&Links="${encodeURIComponent(this.answer_text)}"&Boolean=true&OutputType=HTML#LinkMark`;
-    this.previewUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`${environment.view_api}${url}`);
-  }    
-  
+    url = `?Action=View&NoACI=true&Reference=${encodeURIComponent(
+      url
+    )}&EmbedImages=true&StripScript=true&OriginalBaseURL=true&Links="${encodeURIComponent(
+      this.answer_text
+    )}"&Boolean=true&OutputType=HTML#LinkMark`;
+    this.previewUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      `${environment.view_api}${url}`
+    );
+  }
 
   closePreview() {
     this.rawUrl = undefined;
