28369d8f808eb7401a81c379ca040e68fb3efbb5
Ashley Hunter <vjoseph@opentext.com>
2024-03-07 17:28:57 +0000

    adding loading indicator

--- src/app/services/answer.service.ts
+++ src/app/services/answer.service.ts
@@ -1,52 +1,52 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { IAnswerServerAskResponse} from '../interfaces/IAnswerServerResponse';
 import { Observable, catchError, map, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
-import { IManageResourcesResponse } from '../interfaces/IAnswerServerConversationResponse';
 import { IAnswerServerConversationPrompts } from '../interfaces/IAnswerServerConversationPrompts';
-import { IAnswerServerGetStatusResponse, System } from '../interfaces/IAnswerServerGetStatusResponse';
-import { filter } from 'rxjs/operators';
+import { IManageResourcesResponse } from '../interfaces/IAnswerServerConversationResponse';
+import {
+  IAnswerServerGetStatusResponse,
+  System,
+} from '../interfaces/IAnswerServerGetStatusResponse';
+import { IAnswerServerAskResponse } from '../interfaces/IAnswerServerResponse';
 import { IndexedDbService } from './indexed-db.service';
-import { Database } from '../interfaces/IgetStatus';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AnswerService {
-
-  private _url:string = `${environment.answerserver_api}`;
-  private scheme:string = "https";
-  constructor(private _http: HttpClient, private _indexDBService: IndexedDbService) { 
-    
-
-  }
+  private _url: string = `${environment.answerserver_api}`;
+  private scheme: string = 'https';
+  constructor(
+    private _http: HttpClient,
+    private _indexDBService: IndexedDbService
+  ) {}
   getSessionID(): Observable<IManageResourcesResponse> {
     const operation = {
-      "operation": "add",
-      "type": "conversation_session"
+      operation: 'add',
+      type: 'conversation_session',
     };
-  
+
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       // Include other headers as required by your server
     });
-  
+
     const baseUrl = `${environment.answerserver_api}`;
     const queryParams = `action=ManageResources&SystemName=Conversation&Data=${btoa(JSON.stringify(operation))}&ResponseFormat=simplejson`;
-  
+
     this._url = `${baseUrl}/${queryParams}`;
     return this._http.get<IManageResourcesResponse>(this._url, { headers });
   }
-  
-  converse(sessionID:string, text:string): Observable<IAnswerServerConversationPrompts> {
+
+  converse(sessionID: string, text: string): Observable<IAnswerServerConversationPrompts> {
     let params = new HttpParams()
       .set('action', 'converse')
       .set('SessionId', sessionID)
-      .set('SystemName','Conversation')
+      .set('SystemName', 'Conversation')
       .set('text', text)
       .set('ResponseFormat', 'simplejson');
-      const baseUrl = `${environment.answerserver_api}`;
+    const baseUrl = `${environment.answerserver_api}`;
     return this._http.get<IAnswerServerConversationPrompts>(baseUrl, { params }).pipe(
       catchError(error => {
         console.error('Error fetching data', error);
@@ -54,32 +54,34 @@ export class AnswerService {
       })
     );
   }
-  async ask(question:string, databaseMatch:string){
+  async ask(question: string, databaseMatch: string) {
     let answerSystem: any = await this._indexDBService.getItem('selectedAnswerSystem');
-    console.log("Database Match: ", databaseMatch);
-    console.log("Answer System: ", answerSystem.value);
+    console.log('Database Match: ', databaseMatch);
+    console.log('Answer System: ', answerSystem?.value);
     let params = new HttpParams()
       .set('action', 'ask')
       .set('text', question)
-      .set('SystemNames', answerSystem.value)
+      .set('SystemNames', answerSystem?.value)
       .set('DatabaseMatch', databaseMatch)
       .set('ResponseFormat', 'simplejson');
-      const baseUrl = `${environment.answerserver_api}`;
+    const baseUrl = `${environment.answerserver_api}`;
     return this._http.get<IAnswerServerAskResponse>(baseUrl, { params }).pipe(
       catchError(error => {
         console.error('Error fetching data', error);
         return throwError('Error fetching data');
       })
-    );;
+    );
   }
   getAnswerSystems(): Observable<System[]> {
     const baseUrl = `${environment.answerserver_api}`;
-    let params = new HttpParams()
-      .set('action', 'getstatus')
-      .set('ResponseFormat', 'simplejson');
-    return this._http.get<IAnswerServerGetStatusResponse>(`${baseUrl}`, { params }).pipe(
-      map((response: IAnswerServerGetStatusResponse) => response.autnresponse.responsedata.systems.system)
-      
-    );
+    let params = new HttpParams().set('action', 'getstatus').set('ResponseFormat', 'simplejson');
+    return this._http
+      .get<IAnswerServerGetStatusResponse>(`${baseUrl}`, { params })
+      .pipe(
+        map(
+          (response: IAnswerServerGetStatusResponse) =>
+            response.autnresponse.responsedata.systems.system
+        )
+      );
   }
 }
